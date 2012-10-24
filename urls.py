ab12341eb0c8bf811b65026c85492e76ff75d5d0
Nikos Roussos <nikos@roussos.cc>
2012-10-24 18:11:42 +0300

    add browserid

--- urls.py
+++ urls.py
@@ -2,14 +2,17 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.views.generic.simple import direct_to_template
 
-#from django.contrib import admin
-#admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'sunoss.views.home', name='home'),
 
     #admin
     url(r'^admin/', include(admin.site.urls)),
+
+    #browserid
+    (r'^browserid/', include('django_browserid.urls')),
 )
 
 if settings.SERVE_MEDIA:
